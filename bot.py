854cfe57f7b82938dbb0471f5f0940b87d3e1806
SebboGit <59420885+SebboGit@users.noreply.github.com>
2020-04-22 17:26:21 +0200

    Basic structure

--- bot.py
+++ bot.py
@@ -5,3 +5,15 @@ import random
 class SupportBot:
     negatives = ("don't", "stop", "nothing")
     exits = ("bye", "see you", "quit", "pause", "exit", "goodbye", "later")
+
+    def __init__(self):
+        self.matching = {"how_to_pay_bill": [r".*how.*pay (a|the) bill.*", r".*where.*pay (a|the|my)? bill.*"],
+                         "pay_bill": [r".*want.*pay (a|the|my)? bill.", r".*need.*pay (a|the|my)? bill.*"],
+                         "menu": [r".*(can|could)?.*(have|see|bring)? (the)?.* menu.*"],
+                         "bathroom": [r".*where.*bathroom.*"]}
+
+    def welcome(self):
+        name = input("Hey, my name is Blop. Could you tell me your name, please?\n>")
+
+        task = input(f"Hey {name}, what can I help you with?\n>")
+        task = task.lower()
